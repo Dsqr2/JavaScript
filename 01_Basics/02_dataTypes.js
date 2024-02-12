fe411035abfda86a6eae0d46c234c3b509708237
Darshan Jain <112248767+Dsqr2@users.noreply.github.com>
2024-02-12 19:07:06 +0000

    02_dataTypes added

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,24 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+3); // We are using Node.js not Browser
+
+console.log(3+3); // code readability should be high
+
+console.log("Darshan"); 
+
+let name = "Darshan"
+let age = 21
+let isLoggedIn = true
+
+// number => 2 to the power 53
+// bigint => for very large numbers
+// string => "Darshan" // always use double inverted commas
+// boolean => true/false
+// null => standalone value // Null is Empty
+// undefined => not defined value // in this variable is declared but value is not defined 
+// symbol => unique symbols
+
+// object => null is an object 
+
+console.log(typeof null);
+console.log(typeof undefined);
