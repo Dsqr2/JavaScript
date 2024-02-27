f60ccaf252aaedb3f34c766aef4d986378cfa4f0
Darshan Jain <112248767+Dsqr2@users.noreply.github.com>
2024-02-27 10:57:04 +0000

    03_conversionOperation

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -27,5 +27,44 @@ let number = 33
 
 let stringNumber = String(number);
 
-console.log(number);
-console.log(stringNumber);
+// console.log(number);
+// console.log(stringNumber);
+
+// ****************************Operations******************************
+let value = 3;
+let negValue = -value;
+
+console.log(value);
+console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); // Power of 2 ^ 3 => 8
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+
+console.log((3 + 4) * 5 % 3); 
+
+console.log(+true);  // 1
+console.log(10 + true);  // 11
+console.log(+""); // 0 
+
+let num1, num2, num3;
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+gameCounter++;
+console.log(gameCounter); // 101
